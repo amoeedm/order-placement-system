ba248eb4503b9f0e42784ba076146e974aee7b0c
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-14 21:48:56 +0100

    Update

---
+++ src/main/java/com/crudrest/orderplacement/orderplacementsystem/OrderController.java
@@ -0,0 +1,8 @@
+package com.crudrest.orderplacement.orderplacementsystem;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class OrderController {
+}
