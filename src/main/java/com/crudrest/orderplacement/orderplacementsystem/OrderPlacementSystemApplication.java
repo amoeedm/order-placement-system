8fbf9f47819b35510764f22b0d75e44f00c1d2f5
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-14 21:08:12 +0100

    Initial commit

---
+++ src/main/java/com/crudrest/orderplacement/orderplacementsystem/OrderPlacementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.crudrest.orderplacement.orderplacementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlacementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPlacementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudrest/orderplacement/orderplacementsystem/OrderPlacementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudrest.orderplacement.orderplacementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlacementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
