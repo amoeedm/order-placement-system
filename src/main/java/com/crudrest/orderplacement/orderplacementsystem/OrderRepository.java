4ee645ae131e0dca2ab0420e1c4d9351b3099976
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-14 21:30:55 +0100

    Adding repository

---
+++ src/main/java/com/crudrest/orderplacement/orderplacementsystem/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.crudrest.orderplacement.orderplacementsystem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long>{
+}
