0cf250c99d6f6746cfe30d1656b3765b44b757be
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-14 22:03:32 +0100

    Adding controller items

--- src/main/java/com/crudrest/orderplacement/orderplacementsystem/OrderController.java
+++ src/main/java/com/crudrest/orderplacement/orderplacementsystem/OrderController.java
@@ -1,6 +1,10 @@
 package com.crudrest.orderplacement.orderplacementsystem;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -12,5 +16,12 @@ public class OrderController {
     }
 
     @GetMapping("/orders")
-    List<OrderEntity>
+    List<Order> all(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/orders")
+    Order newOrder(@RequestBody Order newOrder){
+        return repository.save(newOrder);
+    }
 }
