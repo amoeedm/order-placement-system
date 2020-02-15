8b43b546741b65f849dcbad6ece474643f0dacad
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-15 10:36:47 +0100

    First POST successful tested

--- src/main/java/com/crudrest/orderplacement/orderplacementsystem/Order.java
+++ src/main/java/com/crudrest/orderplacement/orderplacementsystem/Order.java
@@ -1,18 +1,19 @@
 package com.crudrest.orderplacement.orderplacementsystem;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name="orders")
 
 @Data
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_id")
     private Long id;
 
+    @Column(name = "customer_name")
     private String customerName;
 }
