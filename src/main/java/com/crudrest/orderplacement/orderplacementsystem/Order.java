5f408096e286be9c69ea7b45e2c315bef0cd62b7
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-15 11:25:46 +0100

    Expanded Entity values and first GET successful

--- src/main/java/com/crudrest/orderplacement/orderplacementsystem/Order.java
+++ src/main/java/com/crudrest/orderplacement/orderplacementsystem/Order.java
@@ -1,8 +1,11 @@
 package com.crudrest.orderplacement.orderplacementsystem;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 @Table(name="orders")
@@ -15,5 +18,36 @@ public class Order {
     private Long id;
 
     @Column(name = "customer_name")
-    private String customerName;
+    private String name;
+
+    @Column(name = "customer_phone")
+    //@Size(min = 8, max = 12)
+    private Integer phoneNumber;
+
+    @Column(name = "customer_email")
+    private String email;
+
+    @Column(name = "address_moving_from")
+    private String addressMovingFrom;
+
+    @Column(name = "address_moving_to")
+    private String addressMovingTo;
+
+    @Column(name = "moving")
+    private Boolean moving;
+
+    @Column(name = "packing")
+    private Boolean packing;
+
+    @Column(name = "cleaning")
+    private Boolean cleaning;
+
+    //@JsonFormat(pattern="dd-MM-yyyy")
+    @Column(name = "service_date")
+    private Date serviceDate;
+
+    @Column(name = "order_note")
+    private String orderNote;
+
+
 }
