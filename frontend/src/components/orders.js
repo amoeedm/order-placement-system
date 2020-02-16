237ec7248ac7924716f4964bb9bdee586b22c9b2
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-16 13:13:59 +0100

    Basic dummy UI completed

--- frontend/src/components/orders.js
+++ frontend/src/components/orders.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React from 'react';
+import {Button} from 'react-bootstrap';
 
  const Orders = ({ orders }) => {
       return (
         <div>
-          <center><h1>Order List</h1></center>
-          <button>New Order</button>
+          <center><h3>ACME Moving Company</h3></center>
+          <p></p>
+          <Button variant="primary">New Order</Button>
+          <p></p>
+          <hr></hr>
+            <table cellpadding="10">
+            <tr>
+              <td><h6>Order Id</h6></td>
+              <td><h6>Service Date</h6></td>
+              <td><h6>Customer Name</h6></td>
+              <td><h6>Phone</h6></td>
+              <td><h6>Email</h6></td>
+              <td><h6>From</h6></td>
+              <td><h6>To</h6></td>
+              <td><h6>Packing</h6></td>
+              <td><h6>Moving</h6></td>
+              <td><h6>Cleaning</h6></td>
+              <td><h6>Note</h6></td>
+              <td></td>
+              <td></td>
+             </tr>
           {orders.map((order) => (
-          <div className="order">
-            <div className="order-body">
-              <h5 className="order-name">Order Id: {order.id}</h5>
-              <h6 className="order-phone">Phone number: {order.phoneNumber}</h6>
-              <h6 className="order-email">Email: {order.email}</h6>
-              <h6 className="order-address-from">Moving From: {order.addressMovingFrom}</h6>
-              <h6 className="order-address-to">Moving To: {order.addressMovingTo}</h6>
-              <h6 className="order-date">Date: {order.serviceDate}</h6>
-              <p className="order-note">Note: {order.orderNote}</p>
-              <p className="order-packing">Packing: {order.packing}</p>
-              <p className="order-moving">Moving: {order.moving}</p>
-              <p className="order-cleaning">Cleaning: {order.cleaning}</p>
-              <button>Delete Order</button>
-              <button>Edit Order</button>
-            </div>
-          </div>
+               <tr>
+              <td><p className="order-name">{order.id}</p></td>
+              <td><p className="order-date">{order.serviceDate}</p></td>
+              <td><p className="order-date">{order.name}</p></td>
+              <td><p className="order-phone">{order.phoneNumber}</p></td>
+              <td><p className="order-email">{order.email}</p></td>
+              <td><p className="order-address-from">{order.addressMovingFrom}</p></td>
+              <td><p className="order-address-to">{order.addressMovingTo}</p></td>
+              <td><p className="order-packing">{order.packing}</p></td>
+              <td><p className="order-moving">{order.moving}</p></td>
+              <td><p className="order-cleaning">{order.cleaning}</p></td>
+              <td><p className="order-note">{order.orderNote}</p></td>
+              <td><Button variant="warning">Edit Order</Button></td>
+              <td><Button variant="danger">Delete Order</Button></td>
+              </tr>
           ))}
+        </table>
         </div>
       )
     };
