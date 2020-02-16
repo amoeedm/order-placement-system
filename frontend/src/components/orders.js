0bd5a3ec3cc0a6afae578e464487b0a4070a84e9
Abdul Moeed Mohammad <abdul.moeed.mohammad@gmail.com>
2020-02-16 06:38:09 +0100

    First frontend view of db

--- frontend/src/components/orders.js
+++ frontend/src/components/orders.js
@@ -3,13 +3,20 @@ import React from 'react'
  const Orders = ({ orders }) => {
       return (
         <div>
-          <center><h1>Contact List</h1></center>
+          <center><h1>Order List</h1></center>
           {orders.map((order) => (
           <div className="order">
             <div className="order-body">
-              <h5 className="order-name">{order.name}</h5>
-              <h6 className="order-email">{order.email}</h6>
-              <p className="order-note">{order.note}</p>
+              <h5 className="order-name">Order Id: {order.id}</h5>
+              <h6 className="order-phone">Phone number: {order.phoneNumber}</h6>
+              <h6 className="order-email">Email: {order.email}</h6>
+              <h6 className="order-address-from">Moving From: {order.addressMovingFrom}</h6>
+              <h6 className="order-address-to">Moving To: {order.addressMovingTo}</h6>
+              <h6 className="order-date">Date: {order.serviceDate}</h6>
+              <p className="order-note">Note: {order.orderNote}</p>
+              <p className="order-packing">Packing: {order.packing}</p>
+              <p className="order-moving">Moving: {order.moving}</p>
+              <p className="order-cleaning">Cleaning: {order.cleaning}</p>
             </div>
           </div>
           ))}
