6bddce36e213c1ecc18b091e71c94f923a267a50
Abdul Moeed Mohammad <45050211+amoeedm@users.noreply.github.com>
2020-02-17 07:56:18 +0100

    Added comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Orders from './components/orders';
 
+//Main component class.
 class App extends Component {
 
     state = {
         orders: []
     }
 
+//GETs all entries in the database.
     componentDidMount() {
         fetch('http://localhost:8080/orders')
         .then(res => res.json())
@@ -16,6 +18,7 @@ class App extends Component {
         .catch(console.log)
     }
 
+//Renders all entries in the dataabase by mapping array of Order components.
     render () {
         return (
             <Orders orders={this.state.orders} />
