abb63b9853d96877e218f26ae0b3e6793a87a21b
Abdul Moeed Mohammad <45050211+amoeedm@users.noreply.github.com>
2020-02-17 08:05:18 +0100

    Added comment

--- src/test/java/com/crudrest/orderplacement/orderplacementsystem/OrderTest.java
+++ src/test/java/com/crudrest/orderplacement/orderplacementsystem/OrderTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
+    //Test for checking that object is created..
     @Test
     void orderObjectIsCreated() {
         Order testOrder = new Order();
