d33d4e17aa6f8ebcadcac28b9ec5c192df8dfff9
Abdul Moeed Mohammad <45050211+amoeedm@users.noreply.github.com>
2020-02-17 08:00:36 +0100

    Added comments

--- frontend/src/components/orders.js
+++ frontend/src/components/orders.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import {deleteOrder, newOrder, editOrder} from '../components/click.js';
-
 import '../components/orders.css';
 
+//Returning Orders based on input orders.
  const Orders = ({ orders }) => {
 
       return (
+       
+       //Top, static view of the database.
         <div>
           <p></p>
           <div id="left">  Order Management System</div>
+       
+          //Button for New Order.
           <div id="right"><Button variant="primary" onClick={newOrder}>New Order</Button></div>
+       
           <p></p>
           <hr></hr>
             <table cellpadding="10">
@@ -29,6 +34,8 @@ import '../components/orders.css';
               <td></td>
               <td></td>
              </tr>
+       
+          //Iterating each order and mapping for rendering attributes.
           {orders.map((order) => (
                <tr>
               <td><p className="order-name">{order.id}</p></td>
@@ -42,6 +49,8 @@ import '../components/orders.css';
               <td><p className="order-moving">{order.moving.toString()}</p></td>
               <td><p className="order-cleaning">{order.cleaning.toString()}</p></td>
               <td><p className="order-note">{order.orderNote}</p></td>
+               
+              //Buttons for editing and deleting current order.
               <td><Button variant="warning" onClick={editOrder}>Edit Order</Button></td>
               <td><Button variant="danger" onClick={() => deleteOrder(order.id)}>Delete Order</Button></td>
               </tr>
