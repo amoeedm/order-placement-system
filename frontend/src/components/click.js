3a1b7f236d053c02c81b7d2cecc0df6b63419aad
Abdul Moeed Mohammad <45050211+amoeedm@users.noreply.github.com>
2020-02-17 08:02:50 +0100

    Added comments

--- frontend/src/components/click.js
+++ frontend/src/components/click.js
@@ -1,4 +1,7 @@
 
+//Function that is invoked by button clicks.
+
+//DELETEs current order by using id and reloads to re-render.
 export const deleteOrder = (id) =>{
     fetch('http://localhost:8080/orders/' + id, {
         method: 'DELETE'
@@ -6,11 +9,13 @@ export const deleteOrder = (id) =>{
     window.location.reload();
 };
 
+//Not done yet.
 export const editOrder = () =>{
     console.log("Buttion not in function yet: Edit");
     alert("Buttion not in function yet: Edit");
 };
 
+//Not done yet.
 export const newOrder = () =>{
      console.log("Buttion not in function yet: New");
      alert("Buttion not in function yet:    New");
